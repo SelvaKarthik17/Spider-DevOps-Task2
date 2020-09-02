141ba0b78a9bebd6ae37678df36b3ec6033c038b
SelvaKarthik17 <57003577+SelvaKarthik17@users.noreply.github.com>
2020-09-02 20:53:08 +0530

    updated crontab command
    (to append instead of modify)

--- virus_crontab.sh
+++ virus_crontab.sh
@@ -6,7 +6,7 @@ alias symptomSchedule="./symptom_auto_create.sh"
 
 if [ "$user" = "corona" ]
 then
-  echo "0 12 * * * symptomSchedule" | crontab -
+ crontab -l | sed "\$a0 12 * * * symptomSchedule" | crontab -
 
 else
   echo "User not authorized to run the script"
