699748f255e44050ff9b506346c4e20b16a6acdc
SelvaKarthik17 <57003577+SelvaKarthik17@users.noreply.github.com>
2020-09-04 15:09:03 +0530

    update

--- symptom_auto_create.sh
+++ symptom_auto_create.sh
@@ -7,54 +7,44 @@ arr=(cough cold fever throatpain drycough sneezing fatigue breathshortness losso
 a=1
 while [ $a -lt 6 ]
 do
-	
-	ts1=$(ls home/d1_patient$a/Symptoms/ | wc -l)
-	ts2=$(ls home/d2_patient$a/Symptoms/ | wc -l)
-	
-	s1=$(expr $ts1 + 1)
-	s2=$(expr $ts2 + 1)
+   b=1
+   while [$b -lt 3 ]
+   do
+	ts$b=$(ls home/d$b_patient$a/Symptoms/ | wc -l)
+
+	s$b=$(expr $ts$b + 1)
 	
 	n=$((RANDOM % 16))
-	echo "${arr[$n]}" >>home/d1_patient$a/Symptoms/symptom_$s1.txt
-	n=$((RANDOM % 16))
-	echo "${arr[$n]}" >>home/d2_patient$a/Symptoms/symptom_$s2.txt
+	echo "${arr[$n]}" >>home/d$b_patient$a/Symptoms/symptom_$s$b.txt
+
 	
-	if [ $s1 -gt 20 ]
+	if [ $s$b -gt 20 ]
 	then
-	  userdel -r -f d1_patient$a
+	  userdel -r -f d$b_patient$a
 	fi
-	
-	if [ $s2 -gt 20 ]
-	then
-	  userdel -r -f d2_patient$a
-	fi   
+      b=$(expr $b + 1)
+   done
 	 
-    a=$(expr $a + 1)
+  a=$(expr $a + 1)
 done
 
-tsd1=$(ls home/doc1/Symptoms/ | wc -l)
-tsd2=$(ls home/doc2/Symptoms/ | wc -l)
+a=1
+while [$a -lt 3 ]
+do
+	tsd$a=$(ls home/doc$a/Symptoms/ | wc -l)
+	sd$a=$(expr $tsd$a + 1)
 
-sd1=$(expr $tsd1 + 1)
-sd2=$(expr $tsd2 + 1)
+	n=$((RANDOM % 16))
+	echo "${arr[$n]}" >>home/doc$a/Symptoms/symptom_$sd$a.txt
 
-n=$((RANDOM % 16))
-echo "${arr[$n]}" >>home/doc1/Symptoms/symptom_$sd1.txt
+	if [ $sd$a -gt 20 ]
+	then
+   	   userdel -r -f doc$a
+	fi
 
-n=$((RANDOM % 16))
-echo "${arr[$n]}" >>home/doc2/Symptoms/symptom_$sd2.txt
+   a=$(expr $a + 1)
+done
 
-
-if [ $sd1 -gt 20 ]
-then
-   userdel -r -f doc1
-fi
-
-
-if [ $sd2 -gt 20 ]
-then
-   userdel -r -f doc2
-fi
 
 
 
